4f21abb3ca398be2fd7d163301d4e8874b77e4d5
Ayush-G09 <ayushgokhle@gmail.com>
2022-12-16 19:02:49 +0530

    fifth

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const bodyParser = require("body-parser");
 const PORT = process.env.SERVER_PORT;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(signUp);
 app.use(loginRoute);
 
