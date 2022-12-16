0247306ee9255dcbb794976be5c133047acd35fb
Ayush-G09 <ayushgokhle@gmail.com>
2022-12-16 18:38:24 +0530

    second

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const env = require("dotenv");
 env.config();
 require("./config/db");
@@ -11,6 +12,7 @@ const PORT = process.env.SERVER_PORT;
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(signUp);
 app.use(loginRoute);
 
