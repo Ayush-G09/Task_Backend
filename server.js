8e460e4dd22caa43ef0c72034a262b0a4691fd6c
Ayush-G09 <83686115+Ayush-G09@users.noreply.github.com>
2022-12-16 19:28:21 +0530

    minor update

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const cors = require("cors");
 const env = require("dotenv");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 env.config();
 require("./config/db");
 const signUp = require("./routes/signUp");
 const loginRoute = require("./routes/login");
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT = process.env.SERVER_PORT;
 
