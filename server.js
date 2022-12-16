3a9838c9697f14fa6e4a7d8025ba40b3f4ae5fa0
Ayush-G09 <ayushgokhle@gmail.com>
2022-12-16 19:18:41 +0530

    six

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const env = require("dotenv");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 env.config();
 require("./config/db");
 const signUp = require("./routes/signUp");
 const loginRoute = require("./routes/login");
 
-const bodyParser = require("body-parser");
+
 const PORT = process.env.SERVER_PORT;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(signUp);
 app.use(loginRoute);
 
